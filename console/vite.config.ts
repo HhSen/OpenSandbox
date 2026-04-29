8a73cbc36be7ae4d9e63dd352edf3e3e367122b0
hehuaisen <hehuaisen@didiglobal.com>
2026-04-29 16:27:04 +0800

    fix(console): allow hosts

--- console/vite.config.ts
+++ console/vite.config.ts
@@ -10,4 +10,10 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  server: {
+    allowedHosts: 'all',
+  },
+  preview: {
+    allowedHosts: 'all',
+  },
 })
